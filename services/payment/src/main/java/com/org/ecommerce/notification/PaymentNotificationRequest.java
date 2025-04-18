4aa0c6b955ee7b5df312e9331d845f0a66324bcc
cherylcy <cherylngai23@gmail.com>
2025-04-18 16:09:15 -0700

    feat: add payment service

---
+++ services/payment/src/main/java/com/org/ecommerce/notification/PaymentNotificationRequest.java
@@ -0,0 +1,15 @@
+package com.org.ecommerce.notification;
+
+import com.org.ecommerce.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstname,
+        String customerLastname,
+        String customerEmail
+) {
+}
---
+++ services/payment/src/main/java/com/org/ecommerce/payment/PaymentMapper.java
@@ -0,0 +1,19 @@
+package com.org.ecommerce.payment;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentMapper {
+
+    public Payment toPayment(PaymentRequest request) {
+        if (request == null) {
+            return null;
+        }
+        return Payment.builder()
+                .id(request.id())
+                .orderId(request.orderId())
+                .paymentMethod(request.paymentMethod())
+                .amount(request.amount())
+                .build();
+    }
+}
